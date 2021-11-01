cb74017f51c1d7f666c6fdfa307c7e54aff7a8b4
ikemura23 <banbara.work@gmail.com>
2021-11-01 18:11:53 +0900

    多言語ファイルをTextに表示する

--- lib/main.dart
+++ lib/main.dart
@@ -107,8 +107,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              AppLocalizations.of(context)!.hello("ikemura"),
             ),
             Text(
               '$_counter',
